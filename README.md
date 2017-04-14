9138c0a54bd793454455380b87a1cb7e5bd246d6
Nicole Dominguez <sodevious.net@gmail.com>
2017-04-14 15:31:48 -0400

    Add html, set up new Vue app

---
+++ README.md
@@ -0,0 +1,1 @@
+https://dribbble.com/shots/3434979-mydomains-Mobile-Views
---
+++ js/application.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#myDomains',
+  data: {
+    domains: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
