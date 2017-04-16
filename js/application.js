4b71bf7280ccac0849c74aa2f82f4015c0416ac7
Nicole Dominguez <sodevious.net@gmail.com>
2017-04-15 20:10:07 -0400

    making a component n stuffs

--- js/application.js
+++ js/application.js
@@ -1,19 +1,38 @@
-var app = new Vue({
+
+var Child = {
+    template: '<div class="modal-mask">A custom component!</div>'
+}
+
+
+new Vue({
   el: '#myDomains',
+  
   data: {
-    domains: [
-      {
-          name: "sodevious.net",
-          registrar: "Namecheap"
-      },
-      {
-          name: "sodevio.us",
-          registrar: "iWantMyName"
-      },
-      {
-          name: "nicoledominguez.com",
-          registrar: "Namecheap"
+    newTask: "",
+    taskList: [],
+    showAddModal: false 
+  },
+  
+  components: {
+   'add-modal': Child
+ },
+
+  //This is where we will hold the methods we want to use in our application
+  methods: {
+    addTask: function() {
+      //trim() is used to remove whitespace from both ends of a string
+      var task = this.newTask.trim();
+      //if task is not an empty string
+      if (task) {
+        //Push an object containing the task to the taskList array
+        this.taskList.push({
+          text: task
+        });
+        //Reset newTask to an empty string so the input field is cleared
+        this.newTask = "";
       }
-    ]
-  }
-})
+    }
+  }  
+  
+});
+
