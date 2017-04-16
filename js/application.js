f188853a5966c8c2fa65027a8b716f164d4fefe2
Nicole Dominguez <sodevious.net@gmail.com>
2017-04-16 12:28:23 -0400

    locally host files instead of using npm, work on modal

--- js/application.js
+++ js/application.js
@@ -1,43 +1,36 @@
+var app = new Vue({
+    el: '#myDomains',
 
-var Child = {
-    template: '<div class="app-new">A custom component!</div>'
-}
+    data: {
+        newItem: "",
+        itemList: [{
+            text: "nicoledominguez.com",
+            registrar: "Namecheap"
+        }],
+        showAddModal: false,
+        registrarName: ""
+    },
 
+    methods: {
+        addItem: function() {
+            var task = this.newItem.trim();
+            var registrar = this.registrarName.trim();
 
-new Vue({
-  el: '#myDomains',
-  
-  data: {
-    newTask: "",
-    taskList: [],
-    showAddModal: false,
-    isActive: false
-  },
-  
-  components: {
-   'add-modal': Child
- },
+            if (task) { //if task is not an empty string
+                this.itemList.push({
+                    text: task,
+                    registrar: registrar
+                });
 
-  methods: {
-    addTask: function() {
-      //trim() is used to remove whitespace from both ends of a string
-      var task = this.newTask.trim();
-      //if task is not an empty string
-      if (task) {
-        //Push an object containing the task to the taskList array
-        this.taskList.push({
-          text: task
-        });
-        //Reset newTask to an empty string so the input field is cleared
-        this.newTask = "";
-      }
-  },
-  toggleModal: function(){
-       this.showAddModal = !this.showAddModal,
-       this.isActive = !this.isActive
-   }
+                this.newItem = "";
+                this.registrarName = "";
+                this.showAddModal = false;
+            }
+        },
 
-  }  
-  
+        toggleModal: function(){
+            this.showAddModal = !this.showAddModal,
+            this.copy = "Add new domain"
+        }
+    }
 });
-
